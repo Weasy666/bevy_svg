0da82868291429627a61fbce32123a451f5ec048
Cyan Kneelawk <kneelawk@gmail.com>
2025-10-06 18:48:01 -0700

    Disable backface culling on 3d svg material

--- src/render/svg3d/plugin.rs
+++ src/render/svg3d/plugin.rs
@@ -1,12 +1,13 @@
+use super::SVG_3D_SHADER_HANDLE;
 use crate::svg::Svg;
 use bevy::{
     app::{App, Plugin},
     asset::{AssetApp, load_internal_asset},
-    pbr::{Material, MaterialPlugin},
+    mesh::MeshVertexBufferLayoutRef,
+    pbr::{Material, MaterialPipeline, MaterialPipelineKey, MaterialPlugin},
+    render::render_resource::{RenderPipelineDescriptor, SpecializedMeshPipelineError},
     shader::{Shader, ShaderRef},
 };
-
-use super::SVG_3D_SHADER_HANDLE;
 
 /// Plugin that renders [`Svg`](crate::svg::Svg)s in 2D
 pub struct RenderPlugin;
@@ -23,5 +24,16 @@ impl Plugin for RenderPlugin {
 impl Material for Svg {
     fn fragment_shader() -> ShaderRef {
         SVG_3D_SHADER_HANDLE.into()
+    }
+
+    fn specialize(
+        _pipeline: &MaterialPipeline,
+        descriptor: &mut RenderPipelineDescriptor,
+        _layout: &MeshVertexBufferLayoutRef,
+        _key: MaterialPipelineKey<Self>,
+    ) -> bevy::prelude::Result<(), SpecializedMeshPipelineError> {
+        descriptor.primitive.cull_mode = None;
+
+        Ok(())
     }
 }
